4d703e6a20632630c8a2dbc2d2a20a22e3d60005
fatemehpourghaemi <86041930+fatemehpourghaemi@users.noreply.github.com>
2023-04-07 10:42:05 +0330

    Create script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,19 @@
-def buildApp() {
-  echo "building the app"
+def buildJar() {
+  echo "building the application..."
+  sh 'mvn package'
 }
 
-def testApp() {
-  echo "Testing the application..." 
+def buildImage() {
+  echo "building the docker image.." 
+	withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+	sh 'docker build -t fatemehpourghaemi82/my-repo:jma-2.0 .'
+	sh "echo $PASS | docker login -u $USER --password-stdin"
+	sh 'docker push fatemehpourghaemi82/my-repo:jma-2.0'
+	}
 }
 
 def deployApp() {
   echo "Deploying the application..."
-  echo "deploying version ${params.VERSION}" 
 }
 
 return this 
